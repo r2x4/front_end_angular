ec4062bfd2ec2854a165858ca986e7d62df27ea5
Rodrigo <rrs230780@gmail.com>
2025-08-29 13:27:08 -0500

    cambio

--- src/app/gifs/pages/dashboard-pages/dashboard-pages.component.ts
+++ src/app/gifs/pages/dashboard-pages/dashboard-pages.component.ts
@@ -1,8 +1,12 @@
+import { CommonModule } from '@angular/common';
 import {  Component } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-pages',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+  ],
   templateUrl: './dashboard-pages.component.html',
 })
 export default class DashboardPagesComponent {}
