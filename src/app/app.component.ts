e1cb79c969cc705a8d386f6c1c1459f107f5a2fc
Rodrigo <rrs230780@gmail.com>
2025-08-29 13:21:41 -0500

    componentes

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class App {
+  protected readonly title = signal('Front_End');
+}
---
+++ src/app/gifs/pages/dashboard-pages/dashboard-pages.component.ts
@@ -0,0 +1,8 @@
+import {  Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-pages',
+  imports: [],
+  templateUrl: './dashboard-pages.component.html',
+})
+export default class DashboardPagesComponent {}
